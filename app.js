e51aa2bde2d73ec77f5010bea9b837dbdcbe7031
alaqsaka <aqsaabidin15@gmail.com>
2022-01-01 22:37:30 +0700

    fix: fix bugs related on cors

--- app.js
+++ app.js
@@ -37,6 +37,11 @@ app.set("view engine", "ejs");
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
   next();
 });
 app.use(logger("dev"));
