60057e3d28bda6d8fc99c948fafffc3915c3837e
alaqsaka <aqsaabidin15@gmail.com>
2022-03-01 20:49:15 +0700

    chore: working on cors bugs

--- app.js
+++ app.js
@@ -34,12 +34,13 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
+  // res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
   );
+
   next();
 });
 app.use(logger("dev"));
