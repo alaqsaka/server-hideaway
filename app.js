bfce6b4fff730a818dfc6cadbb47c6223bec3a81
alaqsaka <aqsaabidin15@gmail.com>
2022-03-01 21:04:22 +0700

    add allow-origin

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(function (req, res, next) {
-  // res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   // res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.setHeader(
