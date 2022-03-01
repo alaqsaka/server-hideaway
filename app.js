d70ff49513e3989b85203a8776791a4115ed1c07
alaqsaka <aqsaabidin15@gmail.com>
2022-03-01 21:25:13 +0700

    allow-headers *

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   // res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "*");
 
   next();
 });
