bcbf976d690c495811e1acef03b5adade0a29b93
alaqsaka <aqsaabidin15@gmail.com>
2022-03-01 20:56:21 +0700

    removing allow-headers

--- app.js
+++ app.js
@@ -36,10 +36,10 @@ app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   // res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Accept, X-Requested-With, remember-me, Authorization, type"
-  );
+  // res.setHeader(
+  //   "Access-Control-Allow-Headers",
+  //   "Content-Type, Accept, X-Requested-With, remember-me, Authorization, type"
+  // );
 
   next();
 });
